e680e1ecfb81375614fbb4807ac6da9abbb8bb98
Klovach <xeinsamkeit@gmail.com>
2023-06-18 02:39:27 -0700

    Create variables.py
    This entry details how to utilize variables in Python. Variables are, in their most basic form, placeholders for values. In Python, initializing variables is made simple.   By assigning a value to a variable, a named reference is created that can be used to retrieve and modify the associated data.

---
+++ variables.py
@@ -0,0 +1,31 @@
+'''
+Python Variables:
+This entry details how to utilize variables.
+Variables are, in their most basic form, placeholders for values. In Python, initializing variables is made simple.  
+By assigning a value to a variable, a named reference is created that can be used to retrieve and modify the associated data.
+'''
+
+# Initializing Variables 
+number = 10 #int
+name = "name" #string
+float = 4.3 #float
+is_alive = False #bool
+
+# Assigning multiple values in one line.
+a, b, c, d = (1, 2.5, 4, 5)
+
+# Printing values. 
+print(a, b, c, d)
+
+# Performing basic arithmetic. 
+a = a + 3 # Addition
+b = b * 3 # Multiplication
+c = c / 2 # Division
+d = d - 4 # Subtraction
+
+# Printing values. 
+print(a, b, c, d)
+
+# Assigning multiple values to multiple variables. 
+x, y, z = (1, 2.5, 4.4)
+
