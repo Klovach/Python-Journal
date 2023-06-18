d848efa0042e079febc23c991176638e6c12ceca
Klovach <xeinsamkeit@gmail.com>
2023-06-18 02:06:15 -0700

    Create helloworld.py
    This is the first entry of the Python Journal. This entry is a common beginner's exercise.
    The purpose of this entry is to allow for ease of cross comparison with C, C#, and Java journals at a glance.

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+'''
+This is the first entry of the Python Journal. This entry is a common beginner's exercise.
+The purpose of this entry is to allow for ease of cross comparison with C, C#, and Java journals at a glance.
+'''
+msg = "Hello World"
+print(msg)
