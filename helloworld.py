0df307c3cee0c328fb1aceab0ef8cf449c3901a5
Klovach <xeinsamkeit@gmail.com>
2023-06-18 02:39:48 -0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,5 @@
 '''
+Hello World: 
 This is the first entry of the Python Journal. This entry is a common beginner's exercise.
 The purpose of this entry is to allow for ease of cross comparison with C, C#, and Java journals at a glance.
 '''
