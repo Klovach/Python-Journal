fb7cf23b5e1cd5b43a6c74e02c7be02f1833b2d0
Klovach <xeinsamkeit@gmail.com>
2023-06-19 01:26:26 -0700

    Added notes on tuples and dictionaries.

---
+++ Dictionaries/dictionaries.py
@@ -0,0 +1,13 @@
+'''
+Dictionaries:
+Dictionaries can be indexed in the same way as lists.
+Dictionaries store pairs of elements. Each pair consists of a key and a value.
+Only immutable objects can be used as dictionary keys, such as strings, numbers, and tuples.
+'''
+people = {
+    "Dave": 24,
+    "Mary": 24,
+    "John": 23
+}
+
+print(people["Dave"])
---
+++ Dictionaries/reference-point.py
@@ -0,0 +1,14 @@
+'''
+Reference Point:
+A journal entry to identify dictionaries, tuples, and lists at a glance.
+'''
+
+# Lists : mutable
+list = [1, 2, 3, 4, 5]
+
+# Dictionaries : key-value pairs
+dict = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five"}
+
+# Tuples : immutable
+tp = (1, 2, 3, 4, 5)
+my_tuple = "one", "two", "three", "four", "five"
